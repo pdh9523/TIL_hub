5d3e6133e353783bcc24074b3381bfc24bc19cf4
박동현 <jinee9523@gmail.com>
2024-01-24 15:29:20 +0900

    lab_0124_문제탐색중

---
+++ baekjoon/bj_10989.py
@@ -0,0 +1,9 @@
+a = int(input())
+test_list = []
+for i in range(a) :
+    test_list.append(int(input()))
+
+test_list.sort()
+
+for j in test_list :
+    print(j)
--- baekjoon/bj_2292.py
+++ baekjoon/bj_2292.py
@@ -1,14 +1,19 @@
-# 1 + 6 + 12 + 18 + 24 + ---
+# 0 - 6 - 18 - 36 - 60
 a = int(input())
 
-n =(a-1)//6
+target = (a-1) / 6
 
-def gause(n) :
-    a=0
-    while True : 
-        if (1+a)*a/2 > n :
-            return a
-        else : 
-            a+=1
+if (a-1) % 6 == 0 :
+    target-= 0.1
+# 0 - 1 - 1+2 - 1+2+3 - 1+2+3+4 - 1+2+3+4+5
+# 19, 37 이렇게 나눠 떨어질 떄 문제 발생 
+n = 1
+while target >= (n+1)*n / 2 :
+    if a == 1 :
+        break
+    n += 1
 
-print(gause(n)+1)
+if a == 1 :
+    print(1)
+else :
+    print(n+1)
