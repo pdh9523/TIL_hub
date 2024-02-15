fdd48ef5627451ded6c8b9789814b5b88ec532fa
박동현 <jinee9523@gmail.com>
2024-02-15 11:16:51 +0900

    구조변경

--- baekjoon/bj_2908.py
+++ baekjoon/bj_2908.py
@@ -1,9 +1,9 @@
-a,b = map(str,input().split())
+a,b = input().split() # 뒤집을거니까 숫자로 안받음
 
-a = int(a[::-1])
-b = int(b[::-1])
+a = a[::-1] # 문자열 뒤집기
+b = b[::-1]
 
-if a > b : 
+if int(a) > int(b) : # 뒤집은 후 정수변환해서 비교 
     print(a)
-else : 
+else :
     print(b)
