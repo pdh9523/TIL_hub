2512958bf048e2911aa19f5e08006993daa80fcf
박동현 <jinee9523@gmail.com>
2024-03-06 17:55:04 +0900

    lab_0306

---
+++ baekjoon/bj_11279.py
@@ -0,0 +1,15 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+q = []
+for _ in range(int(input())):
+    num = int(input())
+    if num == 0 :
+        if q :
+            print(-heapq.heappop(q))
+        else :
+            print(0)
+    else :
+        heapq.heappush(q,-num)
