6e43b35466e3746573b3c07633947ede98e3d16b
박동현 <jinee9523@gmail.com>
2024-03-08 00:59:04 +0900

    home_0308

--- sample.py
+++ sample.py
@@ -1,26 +1,3 @@
-def f(num, i, idx):
-    global max_num
+[a,b,c] = [1,2,3]
 
-    if i == N:
-        if max_num < num:
-            max_num = num
-
-    else:
-        if len(sums[i - 1][idx]) >= 2:
-            if num != max(sums[i - 1][idx]):
-                return
-
-        sums[i-1][idx].append(num)
-        for k in range(2):
-            f(num + triangle[i][idx + k], i + 1, idx + k)
-
-N = int(input())
-triangle = [list(map(int, input().split())) for _ in range(N)]
-
-max_num = 0
-sums = []
-for i in range(N - 1):
-    sums.append([[] for _ in range(i + 1)])
-f(triangle[0][0], 1, 0)
-
-print(max_num)
+print(a,b,c)
