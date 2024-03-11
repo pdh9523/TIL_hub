fcd3b934fc367918563a751431cb1068568a0ab4
박동현 <jinee9523@gmail.com>
2024-03-11 17:56:06 +0900

    home_0311

---
+++ baekjoon/bj_18352.py
@@ -0,0 +1,30 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+N,M,K,X = map(int,input().split())
+tree = [[] for _ in range(N+1)]
+visit = [0] * (N+1)
+for _ in range(M):
+    i,j = map(int,input().split())
+    tree[i].append(j)
+
+q = deque([X])
+visit[X] =1
+while q:
+    s = q.popleft()
+    for item in tree[s]:
+        if visit[item] == 0 :
+            visit[item] = visit[s]+1
+            q.append(item)
+ans = []
+
+for i in range(N+1):
+    if visit[i]-1 == K:
+        ans.append(i)
+if not ans :
+    print(-1)
+else :
+    for i in ans:
+        print(i)
