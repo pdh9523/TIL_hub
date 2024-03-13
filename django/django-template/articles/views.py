0d409db65f0607f4d37534705fb082d070eef8c2
박동현 <jinee9523@gmail.com>
2024-03-13 17:53:31 +0900

    lab_0313

---
+++ django/django-template/articles/views.py
@@ -0,0 +1,43 @@
+import random
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    context = {
+        'name' : "SSAFY"
+    }
+    return render(request, 'articles/index.html', context)
+
+def dinner(request):
+    foods = [
+        '국밥',
+        '국수',
+        '카레',
+        '탕수육',
+    ]
+    picked = random.choice(foods)
+    context = {
+        'foods' : foods,
+        'picked' : picked,
+    }
+    return render(request, 'articles/dinner.html', context)
+
+def search(request):
+    return render(request, 'articles/search.html')
+
+def throw(request):
+    return render(request, 'articles/throw.html')
+
+def catch(request):
+
+    context = {
+        'message' : request.GET.get('message')
+    }
+    return render(request, 'articles/catch.html', context)
+
+def greeting(request, name):
+    context = {
+        'name' : name,
+    }
+    return render(request, 'articles/greeting.html', context)
