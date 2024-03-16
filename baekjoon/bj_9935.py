4e44769618d09febbf4827e004eb321dacbd354a
박동현 <jinee9523@gmail.com>
2024-03-16 17:23:43 +0900

    home_0316

--- baekjoon/bj_9935.py
+++ baekjoon/bj_9935.py
@@ -1,3 +1,7 @@
-a = list(input())
-boom = input()
-stack = []
+N, M = int(input()), int(input())
+
+graph = [[] for _ in range(N+1)]
+
+for _ in range(M) :
+    a,b,l = map(int,input().split())
+    graph[a].append((b,l))
