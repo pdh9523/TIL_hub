221d0af0375cf114252d62a82ceab89a3e2d27b7
박동현 <jinee9523@gmail.com>
2024-03-26 00:26:29 +0900

    home_0326

--- sample.py
+++ sample.py
@@ -1,1 +1,30 @@
-print(sum(map(int,input().split())))
+from collections import deque
+
+N, M = map(int, input().split())
+ocean = [list(map(int, input().split())) for _ in range(N)]   # 0 : 빈칸, 1 : 아기 상어
+
+
+di = [1, -1, 0, 0, 1, 1, -1, -1]
+dj = [0, 0, 1, -1, 1, -1, 1, -1]
+
+most_safe = 0
+for i in range(N):
+    for j in range(M):
+        if ocean[i][j] == 0:   # 빈칸이면, 안전거리를 구한다.
+            queue = deque([[i, j]])
+            visited = [[-1] * M for _ in range(N)]
+            visited[i][j] = 0
+            while queue:
+                y, x = queue.popleft()
+                for k in range(8):
+                    p, q = y+di[k], x+dj[k]
+                    if 0 <= p < N and 0 <= q < M and visited[p][q] == -1:
+                        if ocean[p][q] == 0:                # 비어있는 칸이라면
+                            visited[p][q] = visited[y][x] + 1
+                            queue.append([p, q])                 # 해당 좌표 작업 리스트에 추가
+                        else:                                    # 상어가 있는 칸이라면
+                            if most_safe < visited[y][x] + 1:       # 최대 안전거리 업데이트
+                                most_safe = visited[y][x] + 1
+                            queue = []                      # 작업 리스트를 비워서 while 문 종료시키기
+                            break                           # for 문 종료
+print(most_safe)
--- swea/swea_1486.py
+++ swea/swea_1486.py
@@ -1,23 +1,19 @@
-def backtrack(idx = 0, total = 0, visit = []) :
+def backtrack(idx = 0, total = 0):
     global ans
-
-    if ans <= total:
-        return
-    if idx >= N :
-        if total >= B: 
-            ans = total
-        return
+    if ans <= total: return
     
+    if idx >= N :
+        if total >= B : ans = total
+        return
     backtrack(idx+1, total + staff[idx])
     backtrack(idx+1, total)
 
 for idx in range(int(input())):
     temp = []
     N,B = map(int,input().split())
-    staff = sorted(list(map(int,input().split())))
-    ans = float('inf')
+    staff = list(map(int,input().split()))
+    ans = 200000
     
-
     backtrack()
 
     print(f"#{idx+1} {ans-B}")
