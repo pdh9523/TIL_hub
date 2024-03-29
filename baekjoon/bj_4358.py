20f02073e481791ac68113e3936eb9db3d53af16
박동현 <jinee9523@gmail.com>
2024-03-29 16:50:41 +0900

    lab_0329

--- baekjoon/bj_4358.py
+++ baekjoon/bj_4358.py
@@ -9,7 +9,7 @@ while True:
     if not tree:            # 이건 좀;
         break               # 백준 입력 값 제한 없는경우 try / except EOFEerror 로 받는거 아님? 어이없네
     cnt += 1
-
+    
     dic[tree] = dic.get(tree,0) + 1
 
 for key,value in sorted(dic.items()):
