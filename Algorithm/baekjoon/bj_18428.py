8cdbc82171e0a1b063330ec073ec16c6dd4411f3
박동현 <jinee9523@gmail.com>
2024-04-30 17:53:11 +0900

    lab_0430

--- Algorithm/baekjoon/bj_18428.py
+++ Algorithm/baekjoon/bj_18428.py
@@ -1,66 +1,55 @@
+# 감시를 피했는지 여부에 대한 함수
+def check () :
+    for i in range(N):
+        for j in range(N):
+            x,y = i,j
+            if arr[x][y] == "S":
+                # 가로열에 방해물을 설치했는가 ?
+                for d in -1,1 :
+                    while 0 <= x < N :
+                        if arr[x][y] == "O":
+                            break
+                        if arr[x][y] == "T":
+                            return False
+                        x += d
+                    x = i
+                # 세로열에 방해물을 설치했는가 ?
+                for d in -1,1:
+                    while 0 <= y < N :
+                        if arr[x][y] == "O":
+                            break
+                        if arr[x][y] == "T":
+                            return False
+                        y += d
+                    y = j
+    # return 안당했으면 체크 완료
+    return True
+
+
+# 백트래킹 함수 정의
 def backtrack(i=0,j=0, cnt=3):
-    global ans 
-    # 종료 조건
-    if i == N-1 and j == N-1 :
-        if target_i or target_j :
-            ans = False
-        else :
-            ans = True
+    if i == N-1 and j == N :
+        if cnt == 0 and check():
+            # check()를 통과했으면 exit
+            exit(print("YES"))
         return
-    # 정답 나온 이후 조건은 무시
-    if ans :
-        return
-    
-    # 인덱스 변경 조건
+
     if j == N :
-        i,j = i+1,0
-        
-    check_i, check_j = False, False
-    if cnt and school[i][j] == "X":
-        if i in target_i or j in target_j :
-            if i in target_i :
-                target_i.remove(i)
-                check_i = True
-            if j in target_j :
-                target_j.remove(j)
-                check_j = True
-            backtrack(i,j+1,cnt-1)
-            if check_i :
-                target_i.append(i)
-            if check_j :
-                target_j.append(j)
-
-
+        i += 1
+        j = 0
+    
+    if arr[i][j] == "X" and cnt:
+        arr[i][j] = "O"
+        backtrack(i,j+1,cnt-1)
+        arr[i][j] = "X"
     backtrack(i,j+1,cnt)
 
+
+# 입력
 N = int(input())
+arr = [ input().split() for _ in range(N) ]
 
-teacher_i = []
-teacher_j = []
-
-student_i = []
-student_j = []
-
-school = [ input().split() for _ in range(N) ]
-
-for i in range(N):
-    for j in range(N):
-        if school[i][j] == 'T' :
-            teacher_i.append(i)
-            teacher_j.append(j)
-        if school[i][j] == 'S' :
-            student_i.append(i)
-            student_j.append(j)
-
-target_i, target_j = [], []
-
-for i in range(len(student_i)):
-    if student_i[i] in teacher_i:
-        target_i.append(student_i[i])
-    if student_j[i] in teacher_j:
-        target_j.append(student_j[i])
-
-ans = False
-
+#출력
 backtrack()
-print("YES" if ans else "NO")
+# 위에서 exit 안됐으면 "NO"출력
+print("NO")
