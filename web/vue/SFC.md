b340f19e050e67f4e4ce1705d7042b95d69b630d
박동현 <jinee9523@gmail.com>
2024-05-02 17:57:34 +0900

    lab_0502

---
+++ web/vue/SFC.md
@@ -0,0 +1,7 @@
+# Single-File components 
+컴포넌트의 템플릿, 로직 및 스타일을 하나의 파일로 묶어낸 특수한 파일 형식
+
+Vue SFC는 HTML,CSS 및 JavaScript를 단일 파일로 합친 것으로
+
+template, script 및 style 블록은 하나의 파일에서 컴포넌트의 뷰, 로직 및 스타일을 독립적으로 배치
+
---
+++ web/vue/vue-project/src/components/MyComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>My Component</h2>
+    <MyComponentItem />
+
+  </div>
+</template>
+
+<script setup>
+  import MyComponentItem from '@/components/MyComponentItem.vue'
+</script>
+
+<style scoped>
+
+</style>
