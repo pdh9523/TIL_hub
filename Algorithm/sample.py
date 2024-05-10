dc571d56054e3311b3e2017b7fd40e2581f3adae
박동현 <jinee9523@gmail.com>
2024-05-10 17:51:49 +0900

    solved

--- Algorithm/sample.py
+++ Algorithm/sample.py
@@ -1,4 +1,4 @@
-eng_to_num = {"one":1, "two":2, "three":3, "four":4, "five":5, "six":6, "seven":7, "eight":8, "nine":9, "zero":0}
-
-for i in eng_to_num:
-    print(i)
+a=1
+b="<"
+c=3
+print(eval(f'{a} {b} {c}'))
