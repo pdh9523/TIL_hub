3253a9d3902a66b9bc9c01e559db015d36163df4
박동현 <jinee9523@gmail.com>
2024-06-14 18:41:39 +0900

    home_0614

--- Algorithm/baekjoon/bj_2012.py
+++ Algorithm/baekjoon/bj_2012.py
@@ -3,4 +3,4 @@ input = sys.stdin.readline
 
 N = int(input())
 students = sorted([ int(input()) for _ in range(N)])
-print(sum(map(lambda x : abs(students[x] - (x+1)), range(N))))
+print(sum(map(lambda x : abs(students[x] - (x+1)), range(N))))  
