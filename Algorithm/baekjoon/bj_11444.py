b2840dd2accf3b17ad73607428f69d6942fbc281
박동현 <jinee9523@gmail.com>
2024-07-03 01:23:33 +0900

    home_0702

--- Algorithm/baekjoon/bj_11444.py
+++ Algorithm/baekjoon/bj_11444.py
@@ -1,7 +1,22 @@
+def fibonacci(n):
+    if n in fibo: return fibo[n]
+
+    half = n//2
+
+    if n%2:
+        left = fibonacci(half)
+        right = fibonacci(half+1)
+        fibo[n] = (left**2 + right**2)%dv
+        return fibo[n]
+    else :
+        left = fibonacci(half-1)
+        right = fibonacci(half)
+        fibo[n] = ((2*left+right)*right)%dv
+        return fibo[n]
+    
+
 N = int(input())
-
-fibo = [1,1]
-for i in range(N):
-    fibo.append((fibo[-1]+fibo[-2])%1000000007)
-
-print(fibo[-1])
+fibo = dict()
+dv = 1000000007
+fibo[1], fibo[2] = 1, 1
+print(fibonacci(N))
