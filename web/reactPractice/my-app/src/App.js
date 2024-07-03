dc6a346bfefb7f8988f57a3e84d5f67e10a219c0
박동현 <jinee9523@gmail.com>
2024-07-04 01:06:19 +0900

    home_0704

--- web/reactPractice/my-app/src/App.js
+++ web/reactPractice/my-app/src/App.js
@@ -1,47 +1,35 @@
-import styles from "./App.module.css"
 import {useState, useEffect} from "react"
 
 function App() {
-  const [counter, setCounter] = useState(0)
-  const [keyword, setKeyword] = useState("")
-  const onClick = () => setCounter((prev) => prev+1)
-  const onChange = (event) => setKeyword(event.target.value)
-
-  const getTitle = () => {
-    console.log("i run only once")
-  }
-  useEffect(getTitle, [])
-  // 한번만 실행되었으면 하는 경우 useEffect의 두번째 매개변수에 빈 리스트를 넣으면 된다.  => 지켜볼 대상이 없어서 빈 리스트라는 뜻
+  const [loading, setLoading] = useState(true)
+  const [coins, setCoins] = useState([]);
+  // useEffect가 빈 리스트를 감시하므로 로드 시 한번만 실행될 예정
   useEffect(() => {
-    console.log("I run only once")
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then(response => response.json())
+    .then(json => {
+      setCoins(json)
+      setLoading(false)
+    })
   }, [])
-  // 키워드가 변경되었을떄만 실행되었으면 하는 경우 리스트 안에 키워드를 넣으면 된다.
-  useEffect(() => {
-    console.log("I run when 'keyword' changes")
-  }, [keyword])
+  
 
-  useEffect(() => {
-    console.log("I run when 'counter' changes'")
-  }, [counter])
-  // 두 개 이상의 변수가 변경되었을 떄 실행되었으면 하는 경우 리스트 안에 다 집어넣으면 된다. 
-  useEffect(() =>{ 
-    console.log("I run when 'counter' or 'keyword' changes")
-  }, [counter, keyword])
-  return (  
+
+  return (
     <div>
-      <input 
-        onChange={onChange}
-        value={keyword}
-        type="text"
-        placeholder="Search here..." />
-      <h1 
-        className={styles.Title}> 
-        {counter} 
-      </h1>
-      <button onClick={onClick}>click Me</button>
+      <h1>The Coins! {loading ? "" : `(${coins.length})` }</h1>
+      {loading ? <strong>Loading...</strong> : null}
+      <ul>
+        {coins.map((coin,index) => (
+          <li key={index}>
+            {coin.name} ({coin.symbol}): {coin.quotes.USD.price}USD
+            </li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
 
-
 export default App;
+
+// api.coinpaprika.com/v1/tickers
