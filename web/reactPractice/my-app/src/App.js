c0d07263a022f55951e4b9dd0aa389321045a017
박동현 <jinee9523@gmail.com>
2024-07-05 00:55:39 +0900

    home_0705

--- web/reactPractice/my-app/src/App.js
+++ web/reactPractice/my-app/src/App.js
@@ -1,35 +1,26 @@
-import {useState, useEffect} from "react"
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom"
+import Home from "./routes/Home"
+import Detail from "./routes/Detail"
 function App() {
-  const [loading, setLoading] = useState(true)
-  const [coins, setCoins] = useState([]);
-  // useEffect가 빈 리스트를 감시하므로 로드 시 한번만 실행될 예정
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-    .then(response => response.json())
-    .then(json => {
-      setCoins(json)
-      setLoading(false)
-    })
-  }, [])
-  
-
-
   return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})` }</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <ul>
-        {coins.map((coin,index) => (
-          <li key={index}>
-            {coin.name} ({coin.symbol}): {coin.quotes.USD.price}USD
-            </li>
-        ))}
-      </ul>
-    </div>
+  <Router>
+    <Switch>
+      // 상세 페이지
+      <Route path="/movie/:id">
+        <Detail />
+      </Route>
+      // 메인 페이지
+      <Route path="/">
+        <Home />
+      </Route>
+      
+
+    </Switch>
+  </Router>
   )
 }
-
-export default App;
-
-// api.coinpaprika.com/v1/tickers
+export default App
