61afb7f4cdd9143c1345f7fb6849881b8be370df
박동현 <jinee9523@gmail.com>
2024-07-08 17:02:25 +0900

    lab_0708

--- web/react_nextjs/app/(movies)/movies/[id]/page.tsx
+++ web/react_nextjs/app/(movies)/movies/[id]/page.tsx
@@ -1,9 +1,19 @@
+import { API_URL } from "../../../(home)/page"
 
+async function getMovie(id: string) {
+  const response = await fetch(`${API_URL}/${id}`)
+  return response.json()
+}
 
-export default function MovieDetail({
+export default async function MovieDetail({
   params: {id},
 }: {
-  params : {id: string }
+  params: {id: string }
 }) {
-  return <h1> Movie {id} </h1>
+  const movie = await getMovie(id)
+
+  return (
+  <h1> {movie.title} </h1>
+  
+  )
 }
