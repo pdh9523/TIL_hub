e30d8cd9b9b327b75545b23c92dc82674d6722f3
박동현 <jinee9523@gmail.com>
2024-07-15 23:50:50 +0900

    feat: connect socket

--- web/zoom/src/public/js/app.js
+++ web/zoom/src/public/js/app.js
@@ -1,2 +1,29 @@
-const socket = new WebSocket(`ws://{window.location.host}`);
+const messageList = document.querySelector("ul")
+const messageForm = document.querySelector("form")
 
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+        console.log("connected to Server")
+})
+
+socket.addEventListener("message", (message) => {
+    console.log("New Message:", message.data)
+})
+
+socket.addEventListener("close", () => {
+    console.log("socket closed")
+})
+
+setTimeout(() => {
+    socket.send("hello from the browser")
+}, 10000)
+
+function handleSubmit(event) {
+    event.preventDefault()
+    const input = messageForm.querySelector("input")
+    socket.send(input.value)
+    console.log(input.value)
+    input.value = ""
+}
+messageForm.addEventListener("submit", handleSubmit)
