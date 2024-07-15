8543bed6835e509f093f14e953f8cc7ddb3b9ee6
박동현 <jinee9523@gmail.com>
2024-07-15 23:50:50 +0900

    feat: connect socket

--- web/zoom/src/server.js
+++ web/zoom/src/server.js
@@ -9,7 +9,7 @@ app.set("views", __dirname + "/views")
 app.use("/public", express.static(__dirname+"/public"))
 app.get("/", (req,res) => res.render("home"))
 
-const handleListen = () => console.log("Hello")
+const handleListen = () => console.log("now-localhost:3000")
 
 // it's http server
 const server = http.createServer(app)
@@ -17,10 +17,15 @@ const server = http.createServer(app)
 // it's ws server
 const wss = new WebSocket.Server({server})
 
-function handleConnection(socket) {
-    console.log(socket)
-}
+const sockets = []
 
-wss.on("connection", handleConnection)
+wss.on("connection", (socket) => {
+    sockets.push(socket)
+    socket.on("close", () => console.log("disconnected"))
+    socket.on("message", (message) => {
+        sockets.forEach(aSocket => aSocket.send(message.toString()))
+        socket.send(message.toString())
+    })
+})
 
 server.listen(3000, handleListen)
