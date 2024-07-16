820f04fedd4165f18f5a8dd2b9653c48768f88ce
박동현 <jinee9523@gmail.com>
2024-07-16 18:01:17 +0900

    feat: add socketIO

--- web/zoom/src/server.js
+++ web/zoom/src/server.js
@@ -1,6 +1,7 @@
-import WebSocket from "ws"
+// import WebSocket from "ws"
 import http from "http"
 import express from "express"
+import SocketIO from "socket.io"
 
 const app = express()
 
@@ -9,23 +10,50 @@ app.set("views", __dirname + "/views")
 app.use("/public", express.static(__dirname+"/public"))
 app.get("/", (req,res) => res.render("home"))
 
-const handleListen = () => console.log("now-localhost:3000")
 
 // it's http server
-const server = http.createServer(app)
+const httpServer = http.createServer(app)
+const wsServer = SocketIO(httpServer)
 
-// it's ws server
-const wss = new WebSocket.Server({server})
-
-const sockets = []
-
-wss.on("connection", (socket) => {
-    sockets.push(socket)
-    socket.on("close", () => console.log("disconnected"))
-    socket.on("message", (message) => {
-        sockets.forEach(aSocket => aSocket.send(message.toString()))
-        socket.send(message.toString())
+wsServer.on("connection", socket => {
+    socket.onAny((event) => {
+        console.log(`Socket Event: ${event}`)
+    })
+    socket.on("enter_room", (roomName, done) => {
+        // 들어가기 전
+        console.log(socket.rooms) // {socket.Id}
+        // 입장
+        socket.join(roomName)
+        // 들어간 후
+        console.log(socket.rooms) // {socket.Id, "roomName"}
+        done()
     })
 })
 
-server.listen(3000, handleListen)
+// with ws way
+// it's ws server
+// const wss = new WebSocket.Server({server})
+// const sockets = []
+//
+// wss.on("connection", (socket) => {
+//     sockets.push(socket)
+//     // 소켓이 들어오면서 기본 닉네임을 설정
+//     socket["nickname"] = "Anonymous"
+//     socket.on("close", () => console.log("disconnected"))
+//     socket.on("message", (msg) => {
+//         const message = JSON.parse(msg)
+//         // 메세지의 타입을 구분
+//         switch(message.type) {
+//             // 메시지 칸에서 들어온 경우
+//             case "new_message":
+//                 sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`))
+//                 return
+//             // 닉네임 칸에서 들어온 경우
+//             case "nickname":
+//                 socket["nickname"] = message.payload
+//                 return
+//         }
+//     })
+// })
+
+httpServer.listen(3000)
