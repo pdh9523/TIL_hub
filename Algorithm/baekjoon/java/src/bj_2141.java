21c8e7fb86ce515190c192cb102b8380aacdc444
박동현 <jinee9523@gmail.com>
2024-07-23 22:41:57 +0900

    home_0723

---
+++ Algorithm/baekjoon/java/src/bj_2141.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj_2141 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[N][2];
+
+        for (int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        
+    }
+}
