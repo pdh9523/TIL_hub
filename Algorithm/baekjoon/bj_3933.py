36e11b6c4c67c59cb4e6bb7ccb5542e15e0b6445
박동현 <jinee9523@gmail.com>
2024-11-21 13:19:56 +0900

    lab_1121

---
+++ Algorithm/baekjoon/bj_3933.py
@@ -0,0 +1,4 @@
+import sys; input = sys.stdin.readline
+
+
+MAX_VALUE = 2**15
--- Algorithm/sample.py
+++ Algorithm/sample.py
@@ -1,6 +1,2 @@
-arr = [1,2,3]
-
-for i in arr:
-    if i == 2:
-        i = 5
-print(arr)
+def _(__,___): return __+___ if __ and ___ else ___+__;
+_(1,2)
